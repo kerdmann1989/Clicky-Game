bf853229e3dbacbdf3a7b1da336bc56e51808bf1
Kathy Erdmann <kathryn.erdmann@hsc.utah.edu>
2018-12-16 21:36:10 -0700

    fixed scoring issue

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = (props) => (
   <div class="container">
     <nav class="navbar navbar-light bg-light">
       <span class="center navbar-text">
-        Score: {props.currentScore}  |  High Score: {props.highScore}   
+        Score: {props.currentScore}  |  High Score: {props.highScore}  |  {props.rightWrong} 
       </span>
     </nav>
 </div>
